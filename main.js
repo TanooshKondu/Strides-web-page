6e9aa2eb2cf485fccc2c5980395eb59f649c57a0
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-01 21:46:42 +0530

    updated

--- main.js
+++ main.js
@@ -99,7 +99,4 @@ countdown();
 // next.addEventListener('click', function(){
 //     let items = document.querySelectorAll('.item')
 //     document.querySelector('.slide').appendChild(items[0])
-// })
-
-// prev.addEventListener('click', function(){
-//     let items = document.querySelectorAll('.item'
+// 
