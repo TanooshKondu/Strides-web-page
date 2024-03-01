502b6083020893fe822d1a2c54d7f84c4e21dad9
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-01 21:45:44 +0530

    updated

--- main.js
+++ main.js
@@ -103,5 +103,4 @@ countdown();
 
 // prev.addEventListener('click', function(){
 //     let items = document.querySelectorAll('.item')
-//     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-// })
+//     document.querySelector('.slide').prepend(
