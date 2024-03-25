3d65684a1190833bdb22d22a64226387d884c367
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-25 21:32:01 +0530

    updated

--- main.js
+++ main.js
@@ -89,3 +89,99 @@ function formatTime(time) {
 }
 
 countdown();
+
+
+
+///<----------------->///
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
+ScrollReveal().reveal(".about__container .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__container .about__flex", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about__container .btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// discover container
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card__content", {
+  ...scrollRevealOption,
+  interval: 500,
+  delay: 200,
+});
+
+// blogs container
+ScrollReveal().reveal(".blogs__card", {
+  duration: 1000,
+  interval: 400,
+});
+
+// journals container
+ScrollReveal().reveal(".journals__card", {
+  ...scrollRevealOption,
+  interval: 400,
+});
+
+//<<<<<---------------------Count Down Code ---------------------------------->>>>>>>
+
+function countdown() {
+  const targetDate = new Date('March 2, 2024 00:00:00').getTime();
+  
+  const interval = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+  
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  document.getElementById('days').innerText = formatTime(days);
+  document.getElementById('hours').innerText = formatTime(hours);
+  document.getElementById('minutes').innerText = formatTime(minutes);
+  document.getElementById('seconds').innerText = formatTime(seconds);
+  
+  if (distance < 0) {
+      clearInterval(interval);
+      document.getElementById('countdown').innerText = 'EXPIRED';
+  }
+  }, 1000);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
